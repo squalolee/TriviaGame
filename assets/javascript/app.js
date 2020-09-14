7a1ca377f112b60531fe1d29e0a554901ccd5a92
Solomon <squalolee@gmail.com>
2020-09-14 15:57:24 -0400

    Finished the timer function code.

---
+++ assets/javascript/app.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    // Starting variables for timer= total time user has, interval id to stop the timer, numberRight is the number of right answers, numberWrong is the number of wrong answers, unanswered is the number of unanswered.
+    var timer = 30;
+    var intervalId;
+    var numberRight = 0;
+    var numberWrong = 0;
+    var unanswered = 0;
+
+    function run() {
+        intervalId = setInterval(decrement, 1000);
+    }
+
+    $(window).on("load", hide);
+    // Hides the start button and starts the other functions to begin the quiz
+    $('#start').on('click', function(){
+        $(this).hide();
+        show();
+        run();
+    });
+
+    $('#done').on('click', function(){
+        $('#start').hide();
+        hide();
+        results();
+        stop();
+    });
+
+    // Fuction for the timer dispaly and ticking away each second
+    function decrement(){
+        timer--;
+
+        $("#timer").html(" " + timer + " seconds");
+
+        if (timer === 1) {
+            $("#timer").html(" " + timer + " seconds");
+        }
+        else if (timer === 0) {
+            $('#start').hide();
+            hide();
+            results();
+            stop();
+        }
+    }
+
+    function stop(){
+        clearInterval(intervalId);
+    }
+
+    function hide(){
+        $('.form-group').hide();
+        $('#time').hide();
+        $('#done').hide();
+    }
+
+    function show(){
+        $('.q1').show();
+        $('#time').show();
+        $('#done').show();
+    }
+
+    $('input[type=radio]').on("change", function(){
+        numberRight = $('input[value=right]:checked').length;
+        numberWrong = $('input[value=wrong]:checked').length;
+        unanswered = (5-(numberRight + numberWrong));
+    });
+})
